96e89233cc9d474d8729eb64986c56a4aa42c8c7
Shreyash Bhosale <shreyashbhosale078@gmail.com>
2025-12-30 16:08:43 +0530

    Updated Service

--- src/main/java/com/phegon/phegonbank/auth_users/entity/User.java
+++ src/main/java/com/phegon/phegonbank/auth_users/entity/User.java
@@ -1,11 +1,15 @@
 package com.phegon.phegonbank.auth_users.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.phegon.phegonbank.role.entity.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -15,4 +19,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
+      private String firstName;
+
+      private String lastName;
+      private String phoneNumber;
+
+      @Email
+      @Column(unique = true, nullable = false)
+      @NotBlank(message = "Email is required")
+      private String email;
+
+      private String password;
+      private String profilePictureUrl;
+      private boolean active = true;
+
+      @ManyToMany(fetch = FetchType.EAGER)
+      @JoinTable(
+              name = "users_table",
+              joinColumns = @JoinColumn(name = "user_id"),
+              inverseJoinColumns = @JoinColumn(name = "role_id")
+      )
+      private List<Role> roles;
+
+      @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+      private List<Account> accounts;
 }
