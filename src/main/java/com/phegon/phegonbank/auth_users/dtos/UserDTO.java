a8b0194963d6bacd08eb220075d0331dee8c228d
Shreyash Bhosale <shreyashbhosale078@gmail.com>
2025-12-31 17:28:41 +0530

    Notification

--- src/main/java/com/phegon/phegonbank/auth_users/dtos/UserDTO.java
+++ src/main/java/com/phegon/phegonbank/auth_users/dtos/UserDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.phegon.phegonbank.account.dtos.AccountDTO;
 import com.phegon.phegonbank.role.entity.Role;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
